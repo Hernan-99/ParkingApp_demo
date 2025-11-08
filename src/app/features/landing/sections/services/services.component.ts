0b04f3524b9d1fc6c20a39ebd188bcd2a328eedd
Hernan-99 <hernan24744@gmail.com>
2025-11-07 22:49:08 -0300

    chore: implementacion de card service

--- src/app/features/landing/sections/services/services.component.ts
+++ src/app/features/landing/sections/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardServiceComponent } from "../../components/card-service/card-service.component";
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CardServiceComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
