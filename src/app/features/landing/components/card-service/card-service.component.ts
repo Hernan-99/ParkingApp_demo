c504a1a9ab6a422c22905587ad1045cde89c6eec
Hernan-99 <hernan24744@gmail.com>
2025-11-08 13:17:34 -0300

    update

--- src/app/features/landing/components/card-service/card-service.component.ts
+++ src/app/features/landing/components/card-service/card-service.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
+
 import {
   heroHome,
   heroClock,
--- src/app/features/landing/components/hero/hero.component.ts
+++ src/app/features/landing/components/hero/hero.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    // Animación de título
+    gsap.from('.hero-title', {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: 'power3.out',
+    });
 
+    // Animación de párrafo
+    gsap.from('.hero-subtitle', {
+      opacity: 0,
+      y: 30,
+      duration: 1,
+      delay: 0.5,
+      ease: 'power3.out',
+    });
+  }
 }
