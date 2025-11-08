94b0dead757884f182291f42de13c5f7b48e72cc
Hernan-99 <hernan24744@gmail.com>
2025-11-07 22:49:50 -0300

    chore: update

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from "./components/hero/hero.component";
+import { ServicesComponent } from "./sections/services/services.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, ServicesComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
